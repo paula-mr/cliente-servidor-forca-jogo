6db8ac18ba8295d6011f880e974a0df99be96207
Paula Ribeiro <paula.mribeiro05@gmail.com>
2020-08-24 23:41:31 -0300

    Adds logsexit to interface

--- common.h
+++ common.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 
+void logexit(const char *msg);
 int addressParse(const char *addrstr, const char *portstr, struct sockaddr_storage *storage);
 void addressToString(const struct sockaddr *addr, char *str, size_t strsize);
 int initializeServer(const char *proto, const char *portstr, struct sockaddr_storage *storage)
