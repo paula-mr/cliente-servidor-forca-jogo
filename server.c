90692ca9f6510a2aa04429c8b14d44146b8aae09
Paula Ribeiro <paula.mribeiro05@gmail.com>
2020-08-28 23:43:07 -0300

    Adiciona caracter de formatação

--- server.c
+++ server.c
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
         }
 
         close(clientSocket);
-        printf("Conexão fechada com o cliente.\n");
+        printf("Conexão fechada com o cliente.\n\n");
     }
 
 	exit(EXIT_SUCCESS);
